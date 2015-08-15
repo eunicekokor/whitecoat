d43761e68d6635d9441b6d50f020a92ad7713653
Eunice Emefa Kokor <eek2138@columbia.edu>
2015-08-15 00:45:35 -0700

    fully functional

--- whitecoat/views.py
+++ whitecoat/views.py
@@ -9,8 +9,8 @@ client = TwilioRestClient(ACCOUNT_SID, AUTH_TOKEN)
 
 @app.route("/", methods=["GET","POST"])
 def index():
+  data = {}
   if request.method=="POST":
-    print "\n\n\ngot here"
     text = request.form.get("text", "A message")
 
     name = request.form.get("name", "Eunice")
@@ -19,8 +19,5 @@ def index():
                            to="+17149075336",
                            from_="+15627310871",
                            body=body)
-    print "\n\n\ngot here"
-    return redirect(url_for('index'), name=name)
-  else:
-    data = {}
-    return render_template("index.html", data=data)
+
+  return render_template("index.html", data=data)
